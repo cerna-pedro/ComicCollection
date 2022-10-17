f3798daff3b1af6f4b56dcb8ca37f593c7b3df41
Pedro Cerna <pedro.d.cerna@gmail.com>
2022-10-17 06:10:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ComicCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEnumerable<Comic> mostExpensive =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby Comic.Prices[comic.Issue] descending
+                select comic;
+
+            foreach (Comic comic in mostExpensive)
+            {
+                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:C}");
+            }
+        }
+    }
+}
